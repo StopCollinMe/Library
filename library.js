264513dd27bb11cf9969596e6d63501436529a36
Collin Uemura <collinuemura@gmail.com>
2023-04-30 20:13:25 -0700

    Updated

--- library.js
+++ library.js
@@ -1,10 +1,12 @@
 let myLibrary = [];
 
-function Book(author,title,numberOfPages, read){
+class Book{
+    constructor(author, title, numberOfPages, read){
     this.author = author;
     this.title = title;
     this.numberOfPages = numberOfPages;
     this.read = read;
+    }
 }
 
 function addBookToLibrary(book){
